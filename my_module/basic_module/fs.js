af7d4d3bd73807f7a87bbdbcc817727e238cda27
dalramjwi <dalramjwi@gmail.com>
2024-06-08 00:17:20 +0900

    fs param add

--- my_module/basic_module/fs.js
+++ my_module/basic_module/fs.js
@@ -3,6 +3,11 @@
  */
 const fs = require("fs");
 const fsFunction = {
+  /**
+   * fs.readfile의 함수화, readfile을 한 후 실행할 함수를 콜백함수로 지정했다.
+   * @param {string} path file을 읽어올 경로
+   * @param {function(data)} callback 실행할 콜백함수
+   */
   read: function (path, callback) {
     fs.readFile(`${path}`, (err, data) => {
       if (err) {
