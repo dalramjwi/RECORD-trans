c6135095084a3ed54868ebf32333b090bec3fce6
dalramjwi <dalramjwi@gmail.com>
2024-06-07 21:08:58 +0900

    server.listen console log 변경

--- my_module/serverSet.js
+++ my_module/serverSet.js
@@ -39,9 +39,9 @@ const serverSet = function serverSet(port) {
 
   server.listen(port, (err) => {
     if (err) {
-      console.log("오류:", err);
+      console.log("오류 : ", err);
     } else {
-      console.log(`Server is running on port ${port}`);
+      console.log(`https://localhost:${port}`);
     }
   });
 };
