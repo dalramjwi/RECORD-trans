851c98618adcb048650128d36a9d0706ed137bae
dalramjwi <dalramjwi@gmail.com>
2024-06-07 22:17:02 +0900

    fs module making..

--- my_module/basic_module/fs.js
+++ my_module/basic_module/fs.js
@@ -1,2 +1,8 @@
-const fsFunction = {};
+/**
+ * fs 모듈을 이용한 작용을 function으로 압축해둔 객체
+ */
+const fs = require("fs");
+const fsFunction = {
+  read: function (path, err = (err) => console.log("에러 :", err), data) {},
+};
 module.exports = fsFunction;
