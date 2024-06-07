c9789b7bd86ba28bdc8fbe7d9f2aa4e6fa862427
dalramjwi <dalramjwi@gmail.com>
2024-06-07 17:54:55 +0900

    mimeType.js write

---
+++ my_module/mimeType.js
@@ -0,0 +1,14 @@
+/**
+ *미디어 타입을 명시해둔 객체이다.
+ */
+const mimeType = {
+  ".html": "text/html; charset=utf-8",
+  ".css": "text/css",
+  ".js": "application/javascript",
+  ".json": "application/json",
+  ".ico": "image/x-icon",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".png": "image/png",
+  ".gif": "image/gif",
+};
