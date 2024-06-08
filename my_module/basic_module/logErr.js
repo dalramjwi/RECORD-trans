8f98e3c3f4a80ec5167dcb5383b5a5357147c3a6
dalramjwi <dalramjwi@gmail.com>
2024-06-08 14:39:30 +0900

    logErr make

---
+++ my_module/basic_module/logErr.js
@@ -0,0 +1,9 @@
+/**
+ * 오류를 콘솔에 에러 로그로 출력하는 함수이다.
+ * @param {string} err 오류
+ * @returns console.log("오류 발생 : ", err);
+ */
+const logErr = function (err) {
+  return console.error("오류 발생 : ", err);
+};
+module.exports = logErr;
