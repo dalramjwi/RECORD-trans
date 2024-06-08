2a8af28ccb1f276cdd86e236a8c28e6039c2a131
dalramjwi <dalramjwi@gmail.com>
2024-06-08 23:10:40 +0900

    makePath.js make and use getMethod. getMethod edit finsih

--- my_module/getMethod/getMethod.js
+++ my_module/getMethod/getMethod.js
@@ -13,34 +13,24 @@ const mimeType = require("../mimeType");
 const fsFunction = require("../basic_module/fs");
 const template = require("../basic_module/literalTemplate");
 const templateList = require("../basic_module/getTemplateLIst");
+const makePath = require("../basic_module/makePath");
 
 function getMethod(req, res, filePath, contentType) {
   // console.log(req.url);
   if (req.url === "/") {
     writeHead(res, 200, mimeType[".html"]);
     //titleData.json 읽어와 리스트 생성
-    let path = "./public/jsondata/titleData.json";
+    let path = makePath.publicFolderPath("jsondata", "titleData", "json");
     fsFunction.read(path, (data) => {
-      // fs.readFile("./public/jsondata/titleData.json", (err, data) => {
       const htmlList = templateList(data);
       res.end(template.createTemplate(htmlList));
-      // });
     });
-
     //이외에는 자동으로 해석
   } else {
     fsFunction.read(filePath, (data) => {
       writeHead(res, 200, contentType);
       res.end(data);
     });
-    // fs.readFile(filePath, (err, data) => {
-    //   if (err) {
-    //     logErr(err);
-    //   } else {
-    //     writeHead(res, 200, contentType);
-    //     res.end(data);
-    //   }
-    // });
   }
 }
 module.exports = getMethod;
