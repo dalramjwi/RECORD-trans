9582dc8bfd6eb4fbc51bf915b1e9a8f7073fedb7
dalramjwi <dalramjwi@gmail.com>
2024-06-08 14:48:55 +0900

    logErr des edit

--- my_module/basic_module/logErr.js
+++ my_module/basic_module/logErr.js
@@ -1,7 +1,7 @@
 /**
  * 오류를 콘솔에 에러 로그로 출력하는 함수이다.
  * @param {string} err 오류
- * @returns console.log("오류 발생 : ", err);
+ * @returns console.error("오류 발생 : ", err);
  */
 const logErr = function (err) {
   return console.error("오류 발생 : ", err);
