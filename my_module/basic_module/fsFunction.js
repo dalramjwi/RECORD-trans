3f8a6e9cc0bb996f3b84b882a8e953598e2f7244
dalramjwi <dalramjwi@gmail.com>
2024-06-09 21:08:55 +0900

    add param

--- my_module/basic_module/fsFunction.js
+++ my_module/basic_module/fsFunction.js
@@ -19,6 +19,26 @@ const fsFunction = {
       }
     });
   },
+  /**
+   * fs.readdir 함수화, readdir 한 후 실행할 함수를 콜백함수로 지정했다.
+   * @param {string} path file을 읽어올 경로
+   * @param {function(data)} callback 실행할 콜백함수
+   * @description callback 함수 실행할 때 (data) => {} 식으로 화살표 함수 권장
+   */
+  readDir: function (path, callback) {
+    fs.readdir(`${path}`, (err, data) => {
+      if (err) {
+        logErr(err);
+      } else {
+        callback(data);
+      }
+    });
+  },
+  /**
+   * fs.writefile 함수화
+   * @param {string} path file을 읽어올 경로
+   * @param {string} data 읽어온 data
+   */
   write: function (path, data) {
     fs.writeFile(`${path}`, data, (err) => {
       if (err) {
