705f2eef8646f762c32564661b74086ef9199a82
dalramjwi <dalramjwi@gmail.com>
2024-06-09 15:53:20 +0900

    edit reqUrlSet.js and make reqFunctionSet.js

---
+++ my_module/postMethod/reqFunctionSet.js
@@ -0,0 +1,6 @@
+/**
+ * reqUrlSet의 콜백 함수로 사용되기 위한 객체의 모음이다.
+ */
+const reqFunctionSet = {
+  functionWrite: function () {},
+};
--- my_module/postMethod/reqUrlSet.js
+++ my_module/postMethod/reqUrlSet.js
@@ -1,13 +1,14 @@
 /**
  *postMethod에서 req.url이 특정 주소일 때 콜백 함수를 실행시켜주는 함수이다.
  * @param {object} req server의 request 객체
+ * @param {object} res server의 response 객체
  * @param {string} address 접속할 주소
  * @param {function} callback 일치할 경우 실행할 콜백 함수
  */
 
-const reqUrlSet = function (req, address, callback) {
+const reqUrlSet = function (req, res, address, callback) {
   if (req.url === `/${address}`) {
-    callback(req);
+    callback(req, res);
   }
 };
 module.exports = reqUrlSet;
