44177918a00e9cbf05c21a1504da6cc8dfaab8dd
dalramjwi <dalramjwi@gmail.com>
2024-06-09 15:32:16 +0900

    modify reqUrlSet.js

--- my_module/postMethod/reqUrlSet.js
+++ my_module/postMethod/reqUrlSet.js
@@ -1,5 +1,5 @@
 /**
- *postMethod에서 req.url이 특정 주소일 때 어떠한 것을 실행할 것이라 연결해주는 함수이다.
+ *postMethod에서 req.url이 특정 주소일 때 콜백 함수를 실행시켜주는 함수이다.
  * @param {object} req server의 request 객체
  * @param {string} address 접속할 주소
  * @param {function} callback 일치할 경우 실행할 콜백 함수
@@ -7,7 +7,7 @@
 
 const reqUrlSet = function (req, address, callback) {
   if (req.url === `/${address}`) {
-    callback();
+    callback(req);
   }
 };
 module.exports = reqUrlSet;
