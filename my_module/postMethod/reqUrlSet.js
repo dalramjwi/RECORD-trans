c7927a0e5e217a6159ea2299810deaef34165a46
dalramjwi <dalramjwi@gmail.com>
2024-06-09 15:07:50 +0900

    modify reqUrlSet.js, add function part

--- my_module/postMethod/reqUrlSet.js
+++ my_module/postMethod/reqUrlSet.js
@@ -1,14 +1,13 @@
 /**
- *
+ *postMethod에서 req.url이 특정 주소일 때 어떠한 것을 실행할 것이라 연결해주는 함수이다.
  * @param {object} req server의 request 객체
  * @param {string} address 접속할 주소
- * @return {}
+ * @param {function} callback 일치할 경우 실행할 콜백 함수
  */
-const path = require("path");
 
-const reqUrlSet = function (req, address) {
+const reqUrlSet = function (req, address, callback) {
   if (req.url === `/${address}`) {
+    callback();
   }
 };
 module.exports = reqUrlSet;
-console.log(reqUrlSet());
