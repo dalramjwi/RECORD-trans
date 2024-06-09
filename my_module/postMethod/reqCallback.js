d6d22e7aa69c7b0e2606c065947d22d227587173
dalramjwi <dalramjwi@gmail.com>
2024-06-09 21:42:05 +0900

    add comment

--- my_module/postMethod/reqCallback.js
+++ my_module/postMethod/reqCallback.js
@@ -58,6 +58,11 @@ const reqCallback = {
       }
     });
   },
+  /**
+   * @param {object} req 요청 객체
+   * @param {object} res 응답 객체
+   * @param {string} body POST 방식으로 전송된 데이터
+   */
   callbackSak: function (req, res, body) {
     let refereName = refererUse(req);
     let dirPath = readPath.publicDataPath();
